70466b31909da481ecf070b661b347691129d68d
Khoa Ta <khoata010106@gmail.com>
2025-03-02 09:42:19 -0800

    On branch main create ios-swift app

---
+++ ios-app/ios-app/mainView/Dashboard.swift
@@ -0,0 +1,19 @@
+//
+//  dashboard.swift
+//  ios-app
+//
+//  Created by Khoa on 2/28/25.
+//
+
+import SwiftUI
+
+struct Dashboard: View {
+    var body: some View {
+        
+        Text("Dashboard")
+    }
+}
+
+#Preview {
+    Dashboard()
+}
